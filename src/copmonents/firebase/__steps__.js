1bd7285ded2fc1e5234de7b2dd7d8b57d5177571
NOMAN1802 <mustakimnoman82@gmail.com>
2023-04-20 13:47:57 +0600

    password field type chane to show passowrd

--- src/copmonents/firebase/__steps__.js
+++ src/copmonents/firebase/__steps__.js
@@ -15,3 +15,25 @@
  * 5.set the provider
  **
  **/ 
+
+/**
+ * HOSTING
+ * -----------
+ * 1.one time per pc
+ * npm install -g firebase-tools
+ * 2.firebase login
+ * ------------------
+ * For each projects one time
+ * 1.firebase init
+ * 2.proceed
+ * 3.hosting:firebase(up and down arrow) use space bar to select
+ * 4.existing project
+ * 5.selects the projects carefully
+ * 6.which project as public directory: dist
+ * 7.single page application : yes
+ * 8.continuous deployment: no
+ * 
+ * For every time deploy
+ * 1.npm run build
+ * 2.firebase deploy
+ * **/  
